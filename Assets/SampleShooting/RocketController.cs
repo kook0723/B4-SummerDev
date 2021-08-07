51392872e666f161dac727bd79401d56f16466c6
Toyoshima Kota <kota@Toyoshimas-MacBook-Air.local>
2021-08-07 16:39:25 +0900

    finish 1st section.

---
+++ Assets/SampleShooting/RocketController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (-0.1f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate ( 0.1f, 0, 0);
+		}
+	}
+}
